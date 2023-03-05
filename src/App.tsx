9f8a1783a2f46f428eadcc7bb22c416558d54314
Anderson Vieira <asvieira.br@gmail.com>
2023-03-04 22:28:41 -0300

    chore: add postcss autoprefixer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Task from "./components/Task/Task";
 
+// tailwind css
+import './styles/global.css';
+
 function App() {
   return (
-    <>
+    <div className="w-screen h-screen bg-gray-900 flex-col items-center justify-center text-gray-200">
       <h1>todo.app</h1>
       <Task />
-    </>
+    </div>
   );
 }
 
