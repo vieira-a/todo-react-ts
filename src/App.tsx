6f71c8e2054c7b6e391f05999decbf9f526ddd56
Anderson Vieira <asvieira.br@gmail.com>
2023-03-05 23:10:30 -0300

    chore: add material react

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Task from "./components/Task/Task";
+import AssignmentSharpIcon from '@mui/icons-material/AssignmentSharp';
 
 // tailwind css
 import './styles/global.css';
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="w-screen h-screen bg-gray-900 flex-col items-center justify-center text-gray-200 text-center">
       <header className="py-8">
+        <AssignmentSharpIcon />
         <h1 className="text-5xl text-purple-600 font-bold">todo.app</h1>
       </header>
       <Task />
